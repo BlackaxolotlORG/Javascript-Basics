f34fc3b52683bcae8ff46b74fbe87b40583aa4bf
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2023-09-18 17:29:26 -0600

    starting with js

--- Tut.js
+++ Tut.js
@@ -1,1 +1,17 @@
+alert("Hello world");
 
+
+let age = 25;
+let year = 2019;
+
+console.log(age,year);
+
+age = 30;
+console.log(age);
+
+
+const points = 100;
+
+console.log(points);
+
+//Const make the variable not be able to be overwritten 
