0643b1eabae6e5f11b29b8c25680e05a89eb56d0
Diego Lopez-Araiza Soto <140852928+BlackaxolotlORG@users.noreply.github.com>
2023-09-18 18:01:23 -0600

    Update Tut.js

--- Tut.js
+++ Tut.js
@@ -14,4 +14,53 @@ const points = 100;
 
 console.log(points);
 
+
+
+var score= 75;
+console.log(score);
+
 //Const make the variable not be able to be overwritten 
+//let lets you change variables
+//var is the older version so we wont use them
+//cant start variable with numbers
+
+let email = "Hello mikel doesnt let me learn javascript";
+console.log(email)
+console.log("Hello world");
+
+
+let firstName = "Mikel";
+let lastName = "Fresan";
+//this looks a lot like php
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+//getting characters
+console.log(fullName[0]);
+//getting length
+console.log(fullName.length);
+
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
+
+let index = email.indexOf("n")
+console.log(index);
+
+//common string methods
+let emailu = "Diego@gmail.com";
+let result1 = emailu.lastIndexOf("o");
+console.log(result1);
+
+//this one goes from the first postion which is 0 to position 10
+//here im using the mike doesnt let me study sentence instead of the email i just wrote
+let result2 = email.slice(0,10);
+console.log(result2);
+
+
+//this one goes from position 4 to the number of positions so it goes to 14 instead of 10
+//similar to slice
+let result3 = email.substr(4,10);
+console.log(result3);
